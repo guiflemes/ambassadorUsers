ad7ea7bed7f3a37efb8d59e12ea7882bbe060d4d
guilherme <guilherme.1995lemes@gmail.com>
2022-02-09 22:31:25 -0300

    user logic

--- src/repositories/repositories.go
+++ src/repositories/repositories.go
@@ -6,9 +6,9 @@ import (
 
 type UserRepository interface {
 	GetAll() ([]domain.User, error)
-	GetBy(filter map[string]interface{}) (*domain.User, error)
+	GetById(id string) (*domain.User, error)
 	Store(data *domain.User) error
-	Update(data map[string]interface{}, id string) (*domain.User, error)
+	Update(id string, data map[string]interface{}) (*domain.User, error)
 	Delete(id string) error
 	Authenticate(username, password string) (bool, *domain.User, error)
 }
