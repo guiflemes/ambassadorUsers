baec5ad9cb52016d8e2b06e6cc8e76f6cc726f77
guilherme <guilherme.1995lemes@gmail.com>
2022-02-23 08:16:46 -0300

    LoginService Tests

--- src/services/logic/loginLogic_test.go
+++ src/services/logic/loginLogic_test.go
@@ -1,6 +1,7 @@
 package logic
 
 import (
+	"errors"
 	"testing"
 	"users/src/domain"
 
@@ -18,7 +19,7 @@ func (mock *MockRepository) Authenticate(email string, password string) (bool, *
 	return args.Bool(0), result.(*domain.User), args.Error(2)
 }
 
-func Test_loginService_Authenticate(t *testing.T) {
+func testLoginServiceAuthenticateOk(t *testing.T) {
 	assert := assert.New(t)
 	mockRepo := new(MockRepository)
 	user := domain.User{
@@ -41,4 +42,33 @@ func Test_loginService_Authenticate(t *testing.T) {
 	assert.True(want)
 	assert.Same(want1, &user)
 	assert.Nil(wantErr)
+}
+
+func testLoginServiceAuthenticateFailed(t *testing.T) {
+	assert := assert.New(t)
+	mockRepo := new(MockRepository)
+	user := domain.User{}
+
+	mockRepo.On("Authenticate").Return(false, &user, errors.New("failed to authenticate"))
+
+	testLoginService := NewLoginLogic(mockRepo)
+
+	want, want1, wantErr := testLoginService.Authenticate("any_emai@gmail.com", "anypass")
+
+	mockRepo.AssertExpectations(t)
+
+	assert.False(want)
+	assert.Same(want1, &user)
+	assert.Error(wantErr)
+}
+
+func TestLoginServiceAuthenticateTable(t *testing.T) {
+	for scenario, fn := range map[string]func(t *testing.T){
+		"Authenticate Ok":     testLoginServiceAuthenticateOk,
+		"Authenticate Failed": testLoginServiceAuthenticateFailed,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			fn(t)
+		})
+	}
 }
