195c05230065b2fa1da2e57c3f7bf1c7a935b35e
guilherme <guilherme.1995lemes@gmail.com>
2022-04-21 20:47:28 -0300

    test scripts ok

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,6 +1,34 @@
+#!/bin/bash
+
+LANG_ENG="accepting connections"
+LANG_PT_BR="aceitando conexões"
+
+myfunc()
+{
+    langs=( $LANG_ENG $LANG_PT_BR)
+    v=$(docker exec testdb pg_isready -U postgres) 
+    for l in "${langs[@]}";do
+        if [[ $v == *"$l"* ]]; then
+            return 1
+        else
+            return 2
+        fi
+    done
+}
+
+i=1
+sp="/-\|"
+echo -n ' '
+progress="..."
 while true ; do
-    if docker exec testdb pg_isready -U postgres ; then
+    echo -ne "\rwaiting testdb  \b${sp:i++%${#sp}:1} ${progress}"
+    myfunc
+    if  [ $? -eq "1" ]; then
+        echo -ne "\ntestdb is ready\n"
         go test -v  ./src/...
         break
     fi
+    progress="${progress}."
 done
+
+
