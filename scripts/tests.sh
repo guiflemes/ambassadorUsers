449a15fd04ecbfcec5acf61939d17b5d9e09af38
guilherme <guilherme.1995lemes@gmail.com>
2022-04-24 17:14:23 -0300

    add time out to connect with test database

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -16,20 +16,31 @@ myfunc()
     done
 }
 
+SECONDS_LIMIT=10
 i=1
 sp="/-\|"
 echo -n ' '
 progress="..."
 echo -ne "\n"
+START=$(date +%Y%m%d%H%M%S)
 while true ; do
     echo -ne "\r=== Waiting for test database to connect  \b${sp:i++%${#sp}:1} ${progress}"
     myfunc
     if  [ $? -eq "1" ]; then
-        echo -ne "\n=== Test database is ready\n\n"
+        echo -ne "\n=== Test database is ready. \n\n"
         go test -v  ./src/...
         break
     fi
+
+    CURRENT=$(date +%Y%m%d%H%M%S)
+    limit=$(($CURRENT - $START))
+    if [ $limit -gt $SECONDS_LIMIT ];then
+        echo -ne "\n=== Timed out when trying to connect test database, limit ${SECONDS_LIMIT} seconds. \n\n"
+        break
+    fi
+
     progress="${progress}."
+
 done
 
 
